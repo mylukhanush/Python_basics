46db95a5be6e0933bde20d61dc90d43e1dc9b74a
mylukhanush <mylukhanush103@gmail.com>
2025-04-16 18:03:19 +0530

    practise_day3

---
+++ Day3.py
@@ -0,0 +1,51 @@
+# if,else,elif
+
+# x = 10
+# r = x % 2
+#
+# if r==0:
+#     print("Even")
+#     if x>5:
+#         print("Greater")
+# else:
+#     print("odd")
+# print("Bye")
+
+# x = 6
+# if x==2:
+#     print("two")
+# elif(x==3):
+#     print("Three")
+# elif(x==4):
+#     print("Four")
+#
+# else:
+#     print("WRONG")
+
+
+#  loop
+# x =[1,2,3,4,5,6,7,8,9]
+# for i in x:
+#     print(2*i)
+
+#  range
+# for i in range(1,4,1):
+#     print(2*i)
+
+# pyramid
+# n=int(input("Enter the number of rows:"))
+# for i in range(1,n+1):
+#     print(" "*(n-i),end="")
+#     print("* "*i)
+
+
+# for i in range(6):
+#     print("*" *i)
+
+# lambda
+# square = lambda x: x * x
+# print(square(5))
+
+
+
+
